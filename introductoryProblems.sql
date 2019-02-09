582edd5948252ee6b9b986cf9e2517dbd214d586
Jorge Escobar <jorge.ricardo.escobar@gmail.com>
2019-02-09 11:34:04 -0600

    Adds solution to 3. Sales representatives on introductoryProblems.sql

--- introductoryProblems.sql
+++ introductoryProblems.sql
@@ -7,3 +7,8 @@ SELECT * FROM Shippers;
 -- 2. Certain fields from categories
 SELECT CategoryName, Description FROM Categories;
 
+-- 3. Sales representatives
+SELECT FirstName, LastName, HireDate
+FROM Employees
+WHERE Title = 'Sales representative';
+
