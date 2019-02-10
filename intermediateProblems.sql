e376bb37af68f4fd5d3f18578a8e2ad8db95cc47
Jorge Escobar <jorge.ricardo.escobar@gmail.com>
2019-02-09 20:14:31 -0600

    Adds solution to 21. Total customers per country/city on intermediateProblems.sql

--- intermediateProblems.sql
+++ intermediateProblems.sql
@@ -4,3 +4,9 @@ FROM Products
 JOIN Categories on Products.CategoryID = Categories.CategoryID
 GROUP BY CategoryName
 ORDER BY TotalProducts DESC
+
+-- 21. Total customers per country/city
+SELECT Country, City, COUNT(City) AS TotalCustomers
+FROM Customers
+GROUP BY Country, City
+ORDER BY TotalCustomers DESC
