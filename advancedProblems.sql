a943cb3376388109908bf23d04841cf74bc862de
Jorge Escobar <jorge.ricardo.escobar@gmail.com>
2019-02-10 14:04:50 -0600

    Adds solution to 46. Late orders vs. total orders—percentage on advancedProblems.sql

--- advancedProblems.sql
+++ advancedProblems.sql
@@ -192,3 +192,27 @@ LEFT JOIN LateOrders_CTE ON E.EmployeeID = LateOrders_CTE.EmployeeID
 --WHERE ShippedDate >= RequiredDate
 GROUP BY E.EmployeeID, LastName, TotalOrders_CTE.TotalOrders, LateOrders_CTE.LateOrders
 ORDER BY E.EmployeeID
+
+-- 46. Late orders vs. total orders—percentage
+;WITH LateOrders_CTE AS (Select EmployeeID, LateOrders = Count(*)
+                        From Orders
+                        Where RequiredDate <= ShippedDate
+                        Group By EmployeeID
+),
+ TotalOrders_CTE AS (SELECT EmployeeID,
+                         TotalOrders = Count(*)
+                         From Orders
+                         Group By EmployeeID
+)
+SELECT
+  E.EmployeeID,
+  E.LastName,
+  TotalOrders_CTE.TotalOrders,
+  ISNULL(LateOrders_CTE.LateOrders, 0) AS LateOrders,
+  LateOrdersPercent =
+    ISNULL(LateOrders_CTE.LateOrders, 0) * 1.0 / TotalOrders_CTE.TotalOrders
+FROM Employees AS E
+JOIN TotalOrders_CTE ON E.EmployeeID = TotalOrders_CTE.EmployeeID
+LEFT JOIN LateOrders_CTE ON E.EmployeeID = LateOrders_CTE.EmployeeID
+GROUP BY E.EmployeeID, LastName, TotalOrders_CTE.TotalOrders, LateOrders_CTE.LateOrders
+ORDER BY E.EmployeeID
