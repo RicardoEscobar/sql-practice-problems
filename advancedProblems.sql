40d5194c966ab17fbbdfb9c97f31d71a56864c31
Jorge Escobar <jorge.ricardo.escobar@gmail.com>
2019-02-10 11:02:12 -0600

    Adds solution to 32. High-value customers on advancedProblems.sql

---
+++ advancedProblems.sql
@@ -0,0 +1,17 @@
+-- 32. High-value customers
+SELECT
+  C.CustomerID,
+  C.CompanyName,
+  O.OrderID,
+  TotalOrderAmount = SUM(OD.UnitPrice * OD.Quantity)
+FROM Customers AS C
+JOIN Orders AS O ON C.CustomerID = O.CustomerID
+JOIN OrderDetails AS OD ON O.OrderID = OD.OrderID
+WHERE
+  OrderDate BETWEEN '20160101' AND '20170101'
+GROUP BY
+  C.CompanyName,
+  O.OrderID, C.CustomerID,
+  OD.UnitPrice,
+  OD.Quantity
+HAVING SUM(OD.UnitPrice * OD.Quantity) >= 10000
